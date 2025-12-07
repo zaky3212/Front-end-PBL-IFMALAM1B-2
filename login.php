a94f0447c93258c3fe3298fdb0a4cf1ed0a26a69
zaky3212 <fajarzaky690@gmail.com>
2025-12-07 19:23:23 +0700

    memperbaiki desain login

--- login.php
+++ login.php
@@ -1,30 +1,51 @@
-  <?php include 'koneksi.php'; 
-  ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login - Sistem Pengelolaan Rapat</title>
+    <link rel="stylesheet" href="assets/style2.css">
 
-  <!DOCTYPE html>
-  <html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Login Page</title>
-    <link rel="stylesheet" href="assets/style.css" >
-  </head>
-  <body>
-    <div class="container">
-      <div class="login-box">
-        <h1><span class="highlight"></span>Login</h1>
-        <h2>LOGIN</h2>
-        <p class="subtitle">Enter your credentials to access your account</p>
-        
-        <form method="POST" action="proses_login.php">
-    <input type="email" name="email" placeholder="Masukkan email">
-    <input type="password" name="password" placeholder="Masukkan password">
-    <button type="submit">Login</button>
-  </form>
+    
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css"/>
+</head>
 
+<body>
 
-  </div>
+<div class="container">
+
+    
+    <div class="left-side">
+
+        <h1>Sistem Pengelolaan Rapat</h1>
+        <p>Kelola jadwal, peserta, dan dokumentasi rapat dengan efisien dalam satu platform.</p>
 
     </div>
-  </body>
-  </html>
+
+    
+    <div class="login-box">
+        <h2>Selamat Datang</h2>
+        <p class="subtitle">Masukkan email dan password untuk melanjutkan</p>
+
+        <form method="POST" action="proses_login.php">
+
+           
+            <div class="input-group">
+                <i class="fa-solid fa-envelope"></i>
+                <input id="email" type="email" name="email" placeholder="Email" required>
+            </div>
+
+            
+            <div class="input-group">
+                <i class="fa-solid fa-lock"></i>
+                <input id="password" type="password" name="password" placeholder="Password" required>
+            </div>
+
+            <button type="submit">Masuk</button>
+        </form>
+    </div>
+
+</div>
+
+</body>
+</html>
